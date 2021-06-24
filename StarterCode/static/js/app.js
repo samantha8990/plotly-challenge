88509ebf967fee7e30d20f01597aeecd8f2d1d90
Samantha Sommer <samanthajeddy@gmail.com>
2021-06-24 14:10:53 -0700

    hw

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,18 @@
+
+
+ //When importing json, try using metadata
+
+ d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+
+    console.log(result);
+
+ });
+
+
+
+ 
+ <select id="selDataset" onchange="optionChanged(this.value)"></select>
+
